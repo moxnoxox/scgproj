7099657359885664844874d77c58487e0d75509f
jeeee22 <hyunjee2081@gmail.com>
2025-09-14 22:26:50 +0900

    fix: ad 키 떠있는 시간

--- SCGproject/Assets/Scripts/Player/PlayerMove.cs
+++ SCGproject/Assets/Scripts/Player/PlayerMove.cs
@@ -137,7 +137,7 @@ public class PlayerMove : MonoBehaviour
 
     IEnumerator DisableStartIndicator()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(0.2f);
         if (keyInfo != null)
             keyInfo.is_starting = false;
     }
