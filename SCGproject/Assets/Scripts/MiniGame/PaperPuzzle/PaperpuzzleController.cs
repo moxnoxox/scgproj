eabe53dc0c8d9bc64390ce5cc0815d4a1b616638
moxnoxox <moxnoxox3075@outlook.kr>
2025-10-31 16:55:28 +0900

    2챕터까지 빌드 성공, 파일정리게임은 가다듬을 필요 잇음

--- SCGproject/Assets/Scripts/MiniGame/PaperPuzzle/PaperpuzzleController.cs
+++ SCGproject/Assets/Scripts/MiniGame/PaperPuzzle/PaperpuzzleController.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class PaperpuzzleController : MonoBehaviour
